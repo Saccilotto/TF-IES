9bd3caf155320f5d306c70ade97979fbad3623d6
Saccilotto <andre.sacilotto@acad.pucrs.br>
2020-06-19 19:49:38 -0300

    Configuracoes basicas de janela

---
+++ UML/Casa.java
@@ -0,0 +1,17 @@
+public class Casa {
+
+	private int coluna;
+
+	private int linha;
+
+	private Peças Peça;
+
+	private Casa casa;
+
+	private Tabuleiro tabuleiro;
+
+	public boolean isPeça() {
+		return false;
+	}
+
+}
---
+++ UML/Tabuleiro.java
@@ -0,0 +1,17 @@
+public class Tabuleiro {
+
+	private int largura;
+
+	private int altura;
+
+	private Casa casas[][];
+
+	private String NomeTabuleiro;
+
+	private Peças peças;
+
+	private Xadrez xadrez;
+
+	private Casa casa;
+
+}
