5d19660b3f379d8830e38f8723272c46865ce9b1
Saccilotto <andre.sacilotto@acad.pucrs.br>
2020-06-23 15:24:13 -0300

    Inicio de teste de eventos a partir do menu princiapl do jogo de Xadrez.

--- src/Animacao/painelXadrez.java
+++ src/Animacao/painelXadrez.java
@@ -1,6 +1,7 @@
 package Animacao;
 
 import javafx.application.Application;
+import javafx.event.Event;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -15,10 +16,21 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class painelXadrez extends Application{
-
+	private Stage primaryStage;
+	
+	//acessa o jogo a partir do botao "Jogar"
+	private void disparaJogo(Event e) {
+		System.out.println("Acao a ser concluida");
+		System.out.println(e.toString());
+	}
+	
+	private void saidaJogo() {
+		
+	}
+	
 	@Override
 	public void start(Stage primaryStage) {
-		
+		//criacao do gerenciador de layout gridpane:
 		GridPane root = new GridPane();
 		root.setAlignment(Pos.CENTER);
 		root.setHgap(10);
@@ -26,26 +38,31 @@ public class painelXadrez extends Application{
 		root.setPadding(new Insets(25,25,25,25));
 		//root.setGridLinesVisible(true);
 		
-		//criacao de caixas horizontais pra armazenar botoes e texto, respectivamente.
+		//criacao de caixas horizontais pra armazenar botoes e texto, respectivamente:
 		HBox hbBut = new HBox();
 		HBox hbText = new HBox();
 		
-		//Edicao da caixa horizontal de texto
+		//Edicao da caixa horizontal de texto:
 		hbText.setAlignment(Pos.CENTER);
 		Text login = new Text("Bem Vindo à Chess Battles!");
 		login.setFont(Font.font("Tahoma",FontWeight.EXTRA_BOLD,35));
 		hbText.getChildren().add(login);
 		root.add(hbText,0,0);
 		
-		//Edicao da caixa horizontal de botoes.
+		//Edicao da caixa horizontal de botoes:
 		hbBut.setAlignment(Pos.BOTTOM_CENTER);
 		hbBut.setSpacing(100);
-		//botao jogar deve disparar uma nova cena onde o jogo comecara.(a fazer)
+		
+		//botao jogar dispara uma nova cena onde o jogo comeca:
 		Button butJogar = new Button("Jogar");
-		butJogar.autosize();
-		hbBut.getChildren().add(butJogar);				
+		butJogar.autosize();		
+		butJogar.setOnAction(e->disparaJogo(e));
+		hbBut.getChildren().add(butJogar);
+		//botao sair fecha a janela inicial e termina o jogo:
 		Button butSair = new Button("Sair");
+		butSair.autosize();
 		hbBut.getChildren().add(butSair);
+		//adiciona o hbBut configrado com dois botoes ao gridpane.
 		root.add(hbBut,0,3);
 		
 		Scene scene = new Scene(root);
