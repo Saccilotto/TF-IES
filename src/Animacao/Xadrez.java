c0ca5f30280c8ec7b114084d21b414053a77242b
Saccilotto <andre.sacilotto@acad.pucrs.br>
2020-06-23 22:52:16 -0300

    Finalizacao de testes.

--- src/Animacao/Xadrez.java
+++ src/Animacao/Xadrez.java
@@ -1,8 +1,5 @@
 package Animacao;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.Event;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.text.Font;
@@ -10,33 +7,25 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 
-public class Xadrez extends Application{
-	private Stage primaryStage;	
+
+public class Xadrez extends Application{		
+	private Stage primaryStage;
 	
-	//acessa o jogo a partir do botao "Jogar"
-	private void disparaJogo() {
-		System.out.println("botao Jogar");
+    @Override
+    public void start(Stage primaryStage) {
+    	//metodo de Configuracao do palco
+        configura(primaryStage);
 	}
 	
-	//fecha o jogo a partir do botao "Sair"
-	private void saidaJogo() {
-		stop();	
-	}
-	
-	@Override
-	public void start(Stage primaryStage) {
+	public void configura(Stage primaryStage){
 		this.primaryStage = primaryStage;
-		
 		//criacao de gerenciador de layout Flowpane :
 		FlowPane root = new FlowPane();
 		root.setPadding(new Insets(25,25,25,25));		
@@ -44,7 +33,7 @@ public class Xadrez extends Application{
 		root.setHgap(10);
 		root.setVgap(10);
 		root.setStyle("-fx-background-color: aquamarine; -fx-text-fill: aquamarine;");
-		
+	
 		//criacao de caixas horizontais pra armazenar botoes e texto, respectivamente:
 		HBox hbBut = new HBox();
 		HBox hbText = new HBox();
@@ -64,25 +53,72 @@ public class Xadrez extends Application{
 		Button butJogar = new Button("Jogar");
 		butJogar.setOnAction(e->disparaJogo());		
 		hbBut.getChildren().add(butJogar);
+		
 		//botao sair fecha a janela inicial e termina o jogo:
 		Button butSair = new Button("Sair");
+		/*
+		EventHandler e = new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				Platform.exit();
+			}	
+		butSair.setOnActione(e));
+		*/
 		butSair.setOnAction(e->saidaJogo());
-		hbBut.getChildren().add(butSair);
-		//adiciona o hbBut configrado com dois botoes ao gridpane.
+		hbBut.getChildren().add(butSair);		
+		//adiciona o hbBut configrado com dois botoes ao Flowpane.
 		root.getChildren().add(hbBut);
-		
-		
+					
 		Scene scene = new Scene(root);	
+		//adiconar icones a janela:
 		//primaryStage.getIcons().add(());						
 		primaryStage.setTitle("Chess Battles");
 		primaryStage.setScene(scene);				
 		primaryStage.show();
 	}
-	
-	
-	public void stop() {
-		primaryStage.close();
+		
+	//acessa o jogo a partir do botao "Jogar"
+	private void disparaJogo() {
+		telaJogo(primaryStage);
 	}
+	
+	//fecha o jogo a partir do botao "Sair"
+		private void saidaJogo() {
+			stop();
+				
+	}
+		
+	
+	//tela do Jogo acionada pelo botao Jogar:
+    public void telaJogo(Stage st){
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+        grid.setStyle("-fx-background-color: BURLYWOOD; -fx-text-fill: BURLYWOOD;");
+        
+        /*
+        // Poe o tabuleiro:
+        
+        
+        
+        // Poe as pecas no tabuleiro:
+
+        }
+        */
+        
+        // Monta a cena e exibe        
+        Scene scene = new Scene(grid);
+        st.setScene(scene);
+        st.show();
+     }
+    
+    @Override
+    public void stop() {
+    	//salvar jogo(opcional)
+    	primaryStage.close();
+    }
 
 	public static void main(String[] args) {
 		launch(args);		
