fe535a4cd7065f0d99feb2ea7625d245c51bcb3a
Saccilotto <andre.sacilotto@acad.pucrs.br>
2020-06-30 21:21:34 -0300

    criacao da classe regras

--- src/main/java/logic/Regras.java
+++ src/main/java/logic/Regras.java
@@ -1,15 +1,5 @@
 package main.java.logic;
 
-import main.java.model.Tabuleiro;
-
 public class Regras {
-	/*
-	private static Regras instance;
-	private Tabuleiro t;
-	private Regras r;
 
-	public Regras() {
-
-	}
-*/
 }
